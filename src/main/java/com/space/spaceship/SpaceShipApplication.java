d111afe175738f4a7960b65b8c302ddb6d1bcdc1
ymelnik <yuliia.melnik@capgemini.com>
2024-10-30 23:22:56 +0100

    innit

---
+++ src/main/java/com/space/spaceship/SpaceShipApplication.java
@@ -0,0 +1,18 @@
+package com.space.spaceship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceShipApplication {
+
+  /**
+   * The entry point of application.
+   *
+   * @param args the input arguments
+   */
+  public static void main(String[] args) {
+    SpringApplication.run(SpaceShipApplication.class, args);
+  }
+
+}
